9c7a26e5e96274d4b70e600f4a46ee6865a798d5
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-02-02 20:31:16 +0000

    well

--- src/enemies/spider_chip.gd
+++ src/enemies/spider_chip.gd
@@ -1,1 +1,25 @@
 extends CharacterBody2D
+
+@onready var hurtbox = $Hurtbox
+@onready var animatedsprite = $AnimatedSprite2D
+@onready var animationplayer = $AnimationPlayer
+
+@export var animationname = "RESET"
+
+func _ready():
+	animatedsprite.play("idle")
+	animationplayer.play(animationname)
+
+
+func die():
+	queue_free()
+
+func pain_is_off():
+	hurtbox.set_deferred("monitoring", false)
+
+func pain_is_on():
+	hurtbox.set_deferred("monitoring", true)
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if body.name == "PlayerProjectile":
+		animationplayer.play("DIE")
