13f38d9f1bfef4bc788893d9f7926b32047c1cf3
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-02-08 11:48:45 +0000

    i have new plans

--- src/level objects/pushable_character.gd
+++ src/level objects/pushable_character.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 var push = false
 var direction = 0
 
+enum {NORMAL, DRAGGED}
+
 func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
@@ -12,23 +14,3 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = 0
 	move_and_slide()
-
-func _on_left_side_body_entered(body: Node2D) -> void:
-	if body.name == "PlayerTest":
-		direction = 1
-		push = true
-
-func _on_left_side_body_exited(body: Node2D) -> void:
-	if body.name == "PlayerTest":
-		direction = 0
-		push = false
-
-func _on_right_side_body_entered(body: Node2D) -> void:
-	if body.name == "PlayerTest":
-		direction = -1
-		push = true
-
-func _on_right_side_body_exited(body: Node2D) -> void:
-	if body.name == "PlayerTest":
-		direction = 0
-		push = false
