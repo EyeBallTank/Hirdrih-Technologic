4dbd957a45b9356fd8a2e366dd22fa7b4d80af8a
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-02-10 10:02:26 +0000

    getting close

--- src/level objects/pushable_character.gd
+++ src/level objects/pushable_character.gd
@@ -1,16 +1,81 @@
 extends CharacterBody2D
 
+@onready var animationplayer = $AnimationPlayer
+@onready var hurtbox = $ClickArea
+
 var push = false
 var direction = 0
 
+var canibedragged = false
+
+@export var movement_speed : float = 200
+var character_direction : Vector2
+
+
 enum {NORMAL, DRAGGED}
+
+var state = DRAGGED
+
+func _ready() -> void:
+	animationplayer.play("RESET")
 
 func _physics_process(delta: float) -> void:
 
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	if push:
-		velocity.x = direction*delta*500
-	else:
-		velocity.x = 0
-	move_and_slide()
+	match state:
+		NORMAL:
+
+			if not is_on_floor():
+				velocity += get_gravity() * delta
+			if push:
+				velocity.x = direction*delta*500
+			else:
+				velocity.x = 0
+			move_and_slide()
+			
+			if canibedragged == true:
+				if Input.is_action_just_released("shoot"):
+					animationplayer.play("being_dragged")
+			elif canibedragged == false:
+				pass
+			
+			
+		DRAGGED:
+			character_direction.x = Input.get_axis("left", "right")
+			character_direction.y = Input.get_axis("jump", "climbdown")
+			character_direction = character_direction.normalized()
+
+			if character_direction:
+				velocity = character_direction * movement_speed 
+			else:
+				velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
+
+			move_and_slide()
+
+			if Input.is_action_just_released("shoot"):
+				animationplayer.play("RESET")
+
+			if Input.is_action_just_pressed("activatemouse"):
+				animationplayer.play("RESET")
+
+
+func be_normal():
+	state = NORMAL
+
+func be_dragged():
+	state = DRAGGED
+
+func pain_is_off():
+	hurtbox.set_deferred("monitoring", false)
+
+func pain_is_on():
+	hurtbox.set_deferred("monitoring", true)
+
+
+func _on_click_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("trueclickablearea"):
+		canibedragged = true
+
+
+func _on_click_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("trueclickablearea"):
+		canibedragged = false
