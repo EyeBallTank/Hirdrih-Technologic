dad859ecab7d0bcbc46a212b0c9498ef3d6abe21
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-02-13 21:19:40 +0000

    i guess it works

--- src/level objects/green_barrier.gd
+++ src/level objects/green_barrier.gd
@@ -4,12 +4,18 @@ extends StaticBody2D
 @onready var animationplayer = $AnimationPlayer
 @onready var sprite = $AnimatedSprite2D
 @export var animationname = "ACTIVE"
+@export var otheranimationname = "DEACTIVE"
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	animationplayer.play(animationname)
+	Signals.green_one_button_was_pressed.connect(_Green_Barrier_React_To_One)
+	Signals.green_two_button_was_pressed.connect(_Green_Barrier_React_To_Two)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _Green_Barrier_React_To_One():
+	animationplayer.play(otheranimationname)
+
+func _Green_Barrier_React_To_Two():
+	animationplayer.play(animationname)
--- src/level objects/green_two_button.gd
+++ src/level objects/green_two_button.gd
@@ -1,11 +1,22 @@
 extends Area2D
 
+@onready var animation = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation.play("CLICKED")
+	Signals.green_one_button_was_pressed.connect(_Green_Button_Two_Can_Be_Clicked_Again)
+
+func _Green_Button_Two_Can_Be_Clicked_Again():
+	animation.play("NORMAL")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "PlayerTest":
+		animation.play("CLICKED")
+		Signals.green_two_button_was_pressed.emit()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("trueclickablearea"):
+		animation.play("CLICKED")
+		Signals.green_two_button_was_pressed.emit()
