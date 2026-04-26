8ca87a94228b36d5436cae0204fbb7bc1bd1f832
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-04-26 21:50:41 +0100

    yes

--- src/level objects/bluedoor.gd
+++ src/level objects/bluedoor.gd
@@ -1,8 +1,12 @@
 extends StaticBody2D
 
 @onready var animation = $AnimationPlayer
+@onready var animatedsprite = $AnimatedSprite2D
+@export var spritename = "default"
+
 
 func _ready() -> void:
+	animatedsprite.play(spritename)
 	animation.play("closed")
 
 func die():
