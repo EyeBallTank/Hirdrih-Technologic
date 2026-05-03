8ff94c9c54375d7018fbc68712ba0a15d4dcc138
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-05-03 14:42:42 +0100

    wait

--- src/npc characters/npc_time_decider.gd
+++ src/npc characters/npc_time_decider.gd
@@ -5,11 +5,15 @@ extends Area2D
 @onready var sprite = $Sprite
 @onready var the_name = $CanvasLayer/NameTitle
 @onready var portrait = $CanvasLayer/NPCPortrait
+@onready var speech_balloon = $NpcSpeechBalloonSprite
+
+var talk_to_me = false
 
 func _ready() -> void:
 	textbox.visible = false
 	the_name.visible = false
 	portrait.visible = false
+	speech_balloon.visible = false
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -17,9 +21,11 @@ func _on_body_entered(body: Node2D) -> void:
 		textbox.visible = true
 		the_name.visible = true
 		portrait.visible = true
+		speech_balloon.visible = true
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("playergroup"):
 		textbox.visible = false
 		the_name.visible = false
 		portrait.visible = false
+		speech_balloon.visible = false
