67b25d1ec2d4b5d43b23c87f99f4eed620ecdf73
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-05-05 22:29:00 +0100

    wait

--- src/npc characters/convo_borhr_otto.gd
+++ src/npc characters/convo_borhr_otto.gd
@@ -1,31 +1,36 @@
 extends Area2D
 
-@onready var textbox = $CanvasLayer/NinePatchRect
-@onready var textboxdialogue = $CanvasLayer/NinePatchRect/Label2
+#@onready var textbox = $CanvasLayer/NinePatchRect
+#@onready var textboxdialogue = $CanvasLayer/NinePatchRect/Label2
 @onready var sprite = $Sprite
-@onready var the_name = $CanvasLayer/NameTitle
-@onready var portrait = $CanvasLayer/NPCPortrait
+#@onready var the_name = $CanvasLayer/NameTitle
+#@onready var portrait = $CanvasLayer/NPCPortrait
 @onready var speech_balloon = $NpcSpeechBalloonSprite
 @onready var animation = $AnimationPlayer
+
+@onready var canvaslayer = $CanvasLayer
 
 var talk_to_me = false
 
 func _ready() -> void:
-	textbox.visible = false
-	the_name.visible = false
-	portrait.visible = false
+	canvaslayer.visible = false
+	#textbox.visible = false
+	#the_name.visible = false
+	#portrait.visible = false
 	speech_balloon.visible = false
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("attachtoladder") and talk_to_me == true:
-		textbox.visible = true
-		the_name.visible = true
-		portrait.visible = true
+		#textbox.visible = true
+		#the_name.visible = true
+		#portrait.visible = true
+		canvaslayer.visible = true
 		animation.play("Conversation")
 	elif talk_to_me == false:
-		textbox.visible = false
-		the_name.visible = false
-		portrait.visible = false
+		#textbox.visible = false
+		#the_name.visible = false
+		#portrait.visible = false
+		canvaslayer.visible = false
 
 
 func _on_body_entered(body: Node2D) -> void:
