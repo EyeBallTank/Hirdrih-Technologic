1cf1a2565786016fa89f638fed857c24eb91dcab
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-05-05 21:39:01 +0100

    wait

---
+++ src/npc characters/convo_borhr_otto.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+@onready var textbox = $CanvasLayer/NinePatchRect
+@onready var textboxdialogue = $CanvasLayer/NinePatchRect/Label2
+@onready var sprite = $Sprite
+@onready var the_name = $CanvasLayer/NameTitle
+@onready var portrait = $CanvasLayer/NPCPortrait
+@onready var speech_balloon = $NpcSpeechBalloonSprite
+@onready var animation = $AnimationPlayer
+
+var talk_to_me = false
+
+func _ready() -> void:
+	textbox.visible = false
+	the_name.visible = false
+	portrait.visible = false
+	speech_balloon.visible = false
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("attachtoladder") and talk_to_me == true:
+		textbox.visible = true
+		the_name.visible = true
+		portrait.visible = true
+		animation.play("Conversation")
+	elif talk_to_me == false:
+		textbox.visible = false
+		the_name.visible = false
+		portrait.visible = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("playergroup"):
+
+		speech_balloon.visible = true
+		talk_to_me = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("playergroup"):
+
+		speech_balloon.visible = false
+		talk_to_me = false
