e5d27d60d8dcc1c9abaf311d98517106318904f8
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-05-05 18:37:05 +0100

    well

--- src/level objects/pushable_barrier.gd
+++ src/level objects/pushable_barrier.gd
@@ -1,14 +1,19 @@
 extends StaticBody2D
 
 @onready var animation = $AnimationPlayer
+@onready var sprites = $AnimatedSprite2D
+
+@export var spriteforclosed = "defaulthirdrih"
+@export var spriteforopen = "openhirdrih"
 
 func _ready() -> void:
 	animation.play("RESET")
 	Signals.pushable_button_pressed.connect(_Pushable_Barrier_Down)
-
+	sprites.play(spriteforclosed)
 
 func _Pushable_Barrier_Down():
 	animation.play("opening")
+	sprites.play(spriteforopen)
 
 func die():
 	queue_free()
