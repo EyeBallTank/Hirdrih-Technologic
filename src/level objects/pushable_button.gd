9dde5732ca3b96e4e39adb11a9415dbfb639990c
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-05-05 18:17:04 +0100

    work

--- src/level objects/pushable_button.gd
+++ src/level objects/pushable_button.gd
@@ -4,13 +4,18 @@ extends Area2D
 @onready var animationplayer = $AnimationPlayer
 @onready var sprites = $AnimatedSprite2D
 
+@export var spritenameforactive = "activehirdrih"
+@export var spritenameforinactive = "inactivehirdrih"
+
 func _ready() -> void:
 	animationplayer.play("RESET")
+	sprites.play(spritenameforactive)
 
 func bring_up_the_signal():
 	Signals.pushable_button_pressed.emit()
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "PushableCharacter":
+	if body.is_in_group("thisbatteryispushable"):
 		animationplayer.play("pressed")
+		sprites.play(spritenameforinactive)
