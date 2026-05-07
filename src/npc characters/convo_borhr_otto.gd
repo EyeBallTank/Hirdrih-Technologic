34a7c5000abfd4f3b5dcb4573d4010a859c0fef2
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-05-07 11:27:34 +0100

    well

--- src/npc characters/convo_borhr_otto.gd
+++ src/npc characters/convo_borhr_otto.gd
@@ -14,16 +14,10 @@ var talk_to_me = false
 
 func _ready() -> void:
 	canvaslayer.visible = false
-	#textbox.visible = false
-	#the_name.visible = false
-	#portrait.visible = false
 	speech_balloon.visible = false
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("attachtoladder") and talk_to_me == true:
-		#textbox.visible = true
-		#the_name.visible = true
-		#portrait.visible = true
 		canvaslayer.visible = true
 		#animation.play("Conversation")
 		animation.seek(0)
@@ -31,21 +25,14 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("climbdown"):
 			animation.advance(4)
 	elif talk_to_me == false:
-		#textbox.visible = false
-		#the_name.visible = false
-		#portrait.visible = false
 		canvaslayer.visible = false
-
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("playergroup"):
-
 		speech_balloon.visible = true
 		talk_to_me = true
 
-
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("playergroup"):
-
 		speech_balloon.visible = false
 		talk_to_me = false
