69a3e42069036ab7d27a21b33ba3c5308d45c996
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-05-07 21:08:58 +0100

    seems fine

--- src/npc characters/convo_borhr_otto.gd
+++ src/npc characters/convo_borhr_otto.gd
@@ -10,6 +10,10 @@ extends Area2D
 
 @onready var canvaslayer = $CanvasLayer
 
+@onready var playbuttonnormal = $CanvasLayer/ButtonPlayNormal
+@onready var playbuttonbackwards = $CanvasLayer/ButtonPlayBackwards
+@onready var playbuttonfaster = $CanvasLayer/ButtonPlayFaster
+@onready var playbuttonpause = $CanvasLayer/ButtonPlayPause
 
 var talk_to_me = false
 
@@ -36,6 +40,28 @@ func _physics_process(delta: float) -> void:
 		
 	elif talk_to_me == false:
 		canvaslayer.visible = false
+
+	if Input.is_action_pressed("attachtoladder"):
+		playbuttonnormal.play("pressed")
+	else:
+		playbuttonnormal.play("unpressed")
+
+	if Input.is_action_pressed("zgetsbigger"):
+		playbuttonbackwards.play("pressed")
+	else:
+		playbuttonbackwards.play("unpressed")
+
+	if Input.is_action_pressed("xgetssmaller"):
+		playbuttonfaster.play("pressed")
+	else:
+		playbuttonfaster.play("unpressed")
+
+	if Input.is_action_pressed("changeimage"):
+		playbuttonpause.play("pressed")
+	else:
+		playbuttonpause.play("unpressed")
+
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("playergroup"):
