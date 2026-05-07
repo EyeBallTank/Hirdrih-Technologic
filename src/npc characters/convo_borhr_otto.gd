0e82bf5f2529b15668ef36333dd858f861376ca8
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-05-07 17:25:02 +0100

    wait

--- src/npc characters/convo_borhr_otto.gd
+++ src/npc characters/convo_borhr_otto.gd
@@ -6,7 +6,7 @@ extends Area2D
 #@onready var the_name = $CanvasLayer/NameTitle
 #@onready var portrait = $CanvasLayer/NPCPortrait
 @onready var speech_balloon = $NpcSpeechBalloonSprite
-@onready var animationplaything = $AnimationPlayer
+@onready var animation = $AnimationPlayer
 
 @onready var canvaslayer = $CanvasLayer
 
@@ -16,31 +16,39 @@ var talk_to_me = false
 func _ready() -> void:
 	#animation.get_animation("Conversation")
 	#animation.seek(0)
+	animation.set_speed_scale(1.0)
 	canvaslayer.visible = false
 	speech_balloon.visible = false
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("attachtoladder") and talk_to_me == true:
 		canvaslayer.visible = true
-		#animation.play("Conversation")
+		animation.play("Conversation")
+		
 		#animation.seek(0)
 		#animation.stop()
 		if Input.is_action_just_pressed("climbdown"):
-			jump_to_next_keyframe(animationplaything)
+			#jump_to_next_keyframe(animationplaything)
 			#animation.advance(1)
+			animation.stop()
 			#animation.seek(+1)
+		elif Input.is_action_just_pressed("shoot"):
+			animation.set_speed_scale(2.5)
+		else:
+			animation.set_speed_scale(1.0)
+			
 	elif talk_to_me == false:
 		canvaslayer.visible = false
 
 
 
-func jump_to_next_keyframe(animationplaything, track_id := 0) -> void:
-	var animation: Animation = animationplaything.get_animation(animationplaything.current_animation)
-	for key: int in animation.track_get_key_count(track_id):
-		var key_time: float = animation.track_get_key_time(track_id, key)
-		if key_time > animationplaything.current_animation_position:
-			animationplaything.seek(key_time)
-			break
+#func jump_to_next_keyframe(animationplaything, track_id := 0) -> void:
+	#var animation: Animation = animationplaything.get_animation(animationplaything.current_animation)
+	#for key: int in animation.track_get_key_count(track_id):
+		#var key_time: float = animation.track_get_key_time(track_id, key)
+		#if key_time > animationplaything.current_animation_position:
+			#animationplaything.seek(key_time)
+			#break
 
 #func jump_to_next_keyframe(track_id := 0) -> void:
 	#var animation: Animation = get_animation("Conversation")
@@ -54,7 +62,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("playergroup"):
 		speech_balloon.visible = true
 		talk_to_me = true
-		#animation.seek(0)
+		animation.seek(0)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("playergroup"):
