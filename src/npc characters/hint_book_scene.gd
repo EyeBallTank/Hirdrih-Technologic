530444339f159df170e56ee173686532c13f8eac
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-06-05 16:24:05 +0100

    wait

--- src/npc characters/hint_book_scene.gd
+++ src/npc characters/hint_book_scene.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
 @onready var textbox = $CanvasLayer/NinePatchRect
-@onready var textboxdialogue = $CanvasLayer/NinePatchRect/Label2
+#@onready var textboxdialogue = $CanvasLayer/NinePatchRect/Label2
 @onready var sprite = $Sprite
 
-@export var customtext = "HIRDRIH_TUTORIAL_TEXT_1"
+#@export var customtext = "HIRDRIH_TUTORIAL_TEXT_1"
 @export var customsprite = "hirdrihbox"
 
 func _ready() -> void:
 	textbox.visible = false
-	textboxdialogue.text = str (customtext)
+	#textboxdialogue.text = str (customtext)
 	sprite.play("hirdrihbox")
 
 func _on_body_entered(body: Node2D) -> void:
