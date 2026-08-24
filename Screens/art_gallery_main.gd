d162a4957afa157151b362dc4d6d81ac61218c96
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2026-08-25 00:59:51 +0100

    save

--- Screens/art_gallery_main.gd
+++ Screens/art_gallery_main.gd
@@ -32,7 +32,7 @@ func _on_greklovick_button_pressed() -> void:
 
 
 func _on_krimbstone_button_pressed() -> void:
-	pass # Replace with function body.
+		SceneTransition.load_scene("res://Screens/gallery pages/art_galllery_03_krimbstone.tscn")
 
 
 func _on_lahiurn_button_pressed() -> void:
